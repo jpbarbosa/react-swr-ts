de947121dd007ce88a820c76660f4d4f9dec3e5f
JP Barbosa <jp@jp7.com.br>
2021-06-24 21:38:51 -0300

    Organize structure and remove unnecessary files

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header>React Simple CRUD With SWR And TypeScript</header>
+    </div>
+  );
+};
